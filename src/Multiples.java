03eefcdabca09e86dae78833a4d66a57c763ed98
cemreboz <112690868+cemreboz@users.noreply.github.com>
2024-09-09 16:30:17 -0400

    multiples added

---
+++ src/Multiples.java
@@ -0,0 +1,15 @@
+public class Multiples {
+
+    public static void main(String[] args) {
+
+        int i;
+
+        for(i=3; i<100; i++) {
+            if(i%3==0 || i%5==0) {
+                System.out.println(i);
+            }
+
+        }
+
+    }
+}
